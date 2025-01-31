e587616e97ad61a11d8a9c9f08f1482ae2226b6c
K.Bapireddy <Bapi-Reddy@users.noreply.github.com>
2025-01-31 13:24:24 +0530

    removed the others

--- agent-server/src/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import express, { Express } from "express";
-import { chatWithAgent } from "../controller";
-
-const router = express.Router();
-
-router.post("/chat", chatWithAgent);
-
-export default router;
