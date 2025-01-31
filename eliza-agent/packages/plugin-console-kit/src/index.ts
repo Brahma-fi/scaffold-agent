5ab61e99aef0575a26c00c679a94d8d7a197d1ed
K.Bapireddy <Bapi-Reddy@users.noreply.github.com>
2025-01-31 13:10:37 +0530

    added support as eliza plugin

---
+++ eliza-agent/packages/plugin-console-kit/src/index.ts
@@ -0,0 +1,20 @@
+import { Plugin } from "@elizaos/core";
+import { sendAction } from "./actions/send";
+import { bridgeAction } from "./actions/bridge";
+import { swapAction } from "./actions/swap";
+import { bridgeStatusAction } from "./actions/status";
+import { ConsoleKitService, ConsoleKitConfig } from "./services/console";
+
+const service = new ConsoleKitService({} as ConsoleKitConfig);
+
+export const consoleKitPlugin: Plugin = {
+  name: "console-kit",
+  description: "Provides blockchain operations through ConsoleKit",
+  actions: [sendAction, bridgeAction, swapAction, bridgeStatusAction],
+  evaluators: [],
+  providers: [],
+  services: [service],
+};
+
+export type { ConsoleKitConfig };
+export default consoleKitPlugin;
